588d802ddcfa1dab143ffd3c358bb20a1d1aad42
LiuhaoUltra <155093246+LiuHaoUltra@users.noreply.github.com>
2026-02-25 23:06:52 +0800

    fix(package): modernize Gradle config with pluginManagement and align AGP/Kotlin versions to main project

--- packages/adaptive_platform_ui/android/settings.gradle.kts
+++ packages/adaptive_platform_ui/android/settings.gradle.kts
@@ -1,1 +1,14 @@
-rootProject.name = 'adaptive_platform_ui'
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.library") version "8.7.0"
+        id("org.jetbrains.kotlin.android") version "2.1.0"
+    }
+}
+
+rootProject.name = "adaptive_platform_ui"
